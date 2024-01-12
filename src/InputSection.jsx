e261666992cbda5f39dbf487011dc7b418221ece
rupokghosh <rupokghosh291@gmail.com>
2024-01-12 12:01:32 -0800

    add template & classNames

---
+++ src/InputSection.jsx
@@ -0,0 +1,20 @@
+function InputSection () {
+    return (
+        <>
+        <div className="personalInfoInput">
+            
+        </div>
+        <div className="educationInput">
+
+        </div>
+        <div className="experienceInput">
+
+        </div>
+        <div className="skillInput">
+
+        </div>
+        </>
+    )
+}
+
+export default InputSection;
