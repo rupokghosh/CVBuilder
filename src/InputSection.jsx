96430c023d4d09f4c1a424a6455216b88cf1b89e
rupokghosh <rupokghosh291@gmail.com>
2024-01-14 23:59:42 -0800

    add input;

--- src/InputSection.jsx
+++ src/InputSection.jsx
@@ -1,20 +1,12 @@
-function InputSection () {
-    return (
-        <>
-        <div className="personalInfoInput">
-            
-        </div>
-        <div className="educationInput">
-
-        </div>
-        <div className="experienceInput">
-
-        </div>
-        <div className="skillInput">
-
-        </div>
-        </>
-    )
+function InputSection() {
+  return (
+    <>
+      <div className="personalInfoInput"></div>
+      <div className="educationInput"></div>
+      <div className="experienceInput"></div>
+      <div className="skillInput"></div>
+    </>
+  );
 }
 
 export default InputSection;
