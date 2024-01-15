1d7dbbfb36e9e063b3d1e387973989967aca0622
rupokghosh <rupokghosh291@gmail.com>
2024-01-15 00:12:36 -0800

    add fieldsets to education section

--- src/InputSection.jsx
+++ src/InputSection.jsx
@@ -17,7 +17,26 @@ function InputSection() {
             </fieldset>
         </form>
       </div>
-      <div className="educationInput"></div>
+      <div className="educationInput">
+        <fieldsets>
+            <label htmlFor="schoolName">School Name</label>
+            <input type="text" name="schoolName" id="schoolName" placeholder="Enter school/university" />
+        </fieldsets>
+        <fieldsets>
+            <label htmlFor="degreeName">Degree Name</label>
+            <input type="text" name="degreeName" id="degreeName" placeholder="Enter degree/certificate" />
+        </fieldsets>
+        <fieldsets>
+            <label htmlFor="schoolLocation">Location</label>
+            <input type="text" name="schoolLocation" id="schoolLocation" placeholder="Enter Location" />
+        </fieldsets>
+        <fieldsets>
+            <label htmlFor="startDate">Start Date</label>
+            <input type="date" name="startDate" id="startDate" />
+            <label htmlFor="endDate">End Date</label>
+            <input type="date" name="endDate" id="endDate" />
+        </fieldsets>
+      </div>
       <div className="experienceInput"></div>
       <div className="skillInput"></div>
     </>
