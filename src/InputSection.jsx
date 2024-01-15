b37adbfb567275920c27ba21f695f4a5fd282608
rupokghosh <rupokghosh291@gmail.com>
2024-01-15 00:07:27 -0800

    add personalSection fieldsets

--- src/InputSection.jsx
+++ src/InputSection.jsx
@@ -1,7 +1,22 @@
 function InputSection() {
   return (
     <>
-      <div className="personalInfoInput"></div>
+      <div className="personalInfoInput">
+        <form action="">
+            <fieldset>
+                <label htmlFor="fullName">Full Name</label>
+                <input type="text" name="FullName" id="fullName" />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="Email" id="email" />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="phone">Phone</label>
+                <input type="tel" name="Phone" id="phone" />
+            </fieldset>
+        </form>
+      </div>
       <div className="educationInput"></div>
       <div className="experienceInput"></div>
       <div className="skillInput"></div>
