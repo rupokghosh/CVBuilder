958db2ee03e5abd6bb0496ac966038eaefe9c26e
rupokghosh <rupokghosh291@gmail.com>
2024-01-23 23:35:33 -0800

    remove redundant files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import CV from "./CV";
+import Form from "./components/Form";
+
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <Form></Form>
+        <CV></CV>
+      </main>
       <Footer></Footer>
     </div>
   );
