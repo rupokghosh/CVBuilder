519beba5fc4505007dcf50fd44d0fbf8890d4dbe
rupokghosh <rupokghosh291@gmail.com>
2024-01-31 00:03:21 -0800

    link up props with children components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,18 @@
-function Form() {
+import Experience from "./Experience";
+import PersonalInfo from "./PersonalInfo";
+import Education from "./Education";
+import Skills from "./Skills";
+import { useState } from "react";
 
-    return <>
-    <h1>hi</h1>
+function Form({ formData, setFormData }) {
+  return (
+    <>
+      <PersonalInfo setFormData={setFormData} />
+      <Education setFormData={setFormData} />
+      <Experience setFormData={setFormData} />
+      <Skills setFormData={setFormData} />
     </>
+  );
 }
 
 export default Form;
