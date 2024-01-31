8fc8900d0d1d3fc2409e37a59b7f335034543127
rupokghosh <rupokghosh291@gmail.com>
2024-01-31 00:50:45 -0800

    add states and handleInputChange function

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,36 @@
-function Skills() {
+import React, { useState, useEffect } from "react";
+
+function Skills({ setFormData }) {
+  const [skills, setSkills] = useState({
+    skill: "",
+  });
+
+  const handleInputChange = (fieldName, value) => {
+    setSkills((prevSkills) => ({
+      ...prevSkills,
+      [fieldName]: value,
+    }));
+  };
+
+  useEffect(() => {
+    setFormData((prevData) => ({
+      ...prevData,
+      skills,
+    }));
+  }, [skills, setFormData]);
   return (
     <>
       <div className="skillInput">
         <fieldset>
           <label htmlFor="skill">Skill Name</label>
-          <input type="text" name="skill" id="skill" placeholder="Add skill" />
+          <input
+            type="text"
+            name="skill"
+            id="skill"
+            placeholder="Add skill"
+            value={skills.skill}
+            onChange={(e) => handleInputChange("skill", e.target.value)}
+          />
         </fieldset>
       </div>
       ;
