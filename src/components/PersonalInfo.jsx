bc39a3ffdd7114465404870c32227dab56792526
rupokghosh <rupokghosh291@gmail.com>
2024-01-31 00:20:15 -0800

    add handleInputChange function & useState

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,19 +1,51 @@
+import React, { useState, useEffect } from "react";
+
 function PersonalInfo() {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleInputChange = (fieldName, value) => {
+    setPersonalInfo((prevInfo) => ({
+      ...prevInfo,
+      [fieldName]: value,
+    }));
+  };
   return (
     <>
       <div className="personalInfoInput">
         <form action="">
           <fieldset>
             <label htmlFor="fullName">Full Name</label>
-            <input type="text" name="FullName" id="fullName" />
+            <input
+              type="text"
+              name="FullName"
+              id="fullName"
+              value={personalInfo.fullName}
+              onChange={(e) => handleInputChange("fullName", e.target.value)}
+            />
           </fieldset>
           <fieldset>
             <label htmlFor="email">Email</label>
-            <input type="email" name="Email" id="email" />
+            <input
+              type="email"
+              name="Email"
+              id="email"
+              value={personalInfo.email}
+              onChange={(e) => handleInputChange("email", e.target.value)}
+            />
           </fieldset>
           <fieldset>
             <label htmlFor="phone">Phone</label>
-            <input type="tel" name="Phone" id="phone" />
+            <input
+              type="tel"
+              name="Phone"
+              id="phone"
+              value={personalInfo.phone}
+              onChange={(e) => handleInputChange("phone", e.target.value)}
+            />
           </fieldset>
         </form>
       </div>
