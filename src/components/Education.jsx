334d51b3ef4361831983b9bb1e00812b7a41a5f1
rupokghosh <rupokghosh291@gmail.com>
2024-01-31 00:26:30 -0800

    add useState, useEffect and handleInputChange

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,42 +1,83 @@
-function Education() {
+// Education.jsx
+import React, { useState, useEffect } from 'react';
+
+function Education({ setFormData }) {
+  const [education, setEducation] = useState({
+    schoolName: '',
+    degreeName: '',
+    schoolLocation: '',
+    startDate: '',
+    endDate: '',
+  });
+
+  const handleInputChange = (fieldName, value) => {
+    setEducation((prevEducation) => ({
+      ...prevEducation,
+      [fieldName]: value,
+    }));
+  };
+
+  useEffect(() => {
+    setFormData((prevData) => ({
+      ...prevData,
+      education,
+    }));
+  }, [education, setFormData]);
+
   return (
-    <>
-      <div className="educationInput">
-        <fieldset>
-          <label htmlFor="schoolName">School Name</label>
-          <input
-            type="text"
-            name="schoolName"
-            id="schoolName"
-            placeholder="Enter school/university"
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="degreeName">Degree Name</label>
-          <input
-            type="text"
-            name="degreeName"
-            id="degreeName"
-            placeholder="Enter degree/certificate"
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="schoolLocation">Location</label>
-          <input
-            type="text"
-            name="schoolLocation"
-            id="schoolLocation"
-            placeholder="Enter Location"
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="startDate">Start Date</label>
-          <input type="date" name="startDate" id="startDate" />
-          <label htmlFor="endDate">End Date</label>
-          <input type="date" name="endDate" id="endDate" />
-        </fieldset>
-      </div>
-    </>
+    <div className="educationInput">
+      <fieldset>
+        <label htmlFor="schoolName">School Name</label>
+        <input
+          type="text"
+          name="schoolName"
+          id="schoolName"
+          value={education.schoolName}
+          onChange={(e) => handleInputChange('schoolName', e.target.value)}
+          placeholder="Enter school/university"
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="degreeName">Degree Name</label>
+        <input
+          type="text"
+          name="degreeName"
+          id="degreeName"
+          value={education.degreeName}
+          onChange={(e) => handleInputChange('degreeName', e.target.value)}
+          placeholder="Enter degree/certificate"
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="schoolLocation">Location</label>
+        <input
+          type="text"
+          name="schoolLocation"
+          id="schoolLocation"
+          value={education.schoolLocation}
+          onChange={(e) => handleInputChange('schoolLocation', e.target.value)}
+          placeholder="Enter Location"
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="startDate">Start Date</label>
+        <input
+          type="date"
+          name="startDate"
+          id="startDate"
+          value={education.startDate}
+          onChange={(e) => handleInputChange('startDate', e.target.value)}
+        />
+        <label htmlFor="endDate">End Date</label>
+        <input
+          type="date"
+          name="endDate"
+          id="endDate"
+          value={education.endDate}
+          onChange={(e) => handleInputChange('endDate', e.target.value)}
+        />
+      </fieldset>
+    </div>
   );
 }
 
