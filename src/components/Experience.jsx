60185a1f4c003cc61d21ab128a0f4de7f834f6e0
rupokghosh <rupokghosh291@gmail.com>
2024-01-31 00:29:08 -0800

    add return statement that was missed before

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,30 +1,35 @@
 function Experience() {
-  <div className="experienceInput">
-    <fieldset>
-      <label htmlFor="companyName">Company</label>
-      <input
-        type="text"
-        name="companyName"
-        id="companyName"
-        placeholder="Enter company"
-      />
-    </fieldset>
-    <fieldset>
-      <label htmlFor="positionName">Position Title</label>
-      <input
-        type="text"
-        name="positionName"
-        id="positionName"
-        placeholder="Enter position title"
-      />
-    </fieldset>
-    <fieldset>
-      <label htmlFor="startDate">Start Date</label>
-      <input type="date" name="startDate" id="startDate" />
-      <label htmlFor="endDate">End Date</label>
-      <input type="date" name="endDate" id="endDate" />
-    </fieldset>
-  </div>;
+  return (
+    <>
+      <div className="experienceInput">
+        <fieldset>
+          <label htmlFor="companyName">Company</label>
+          <input
+            type="text"
+            name="companyName"
+            id="companyName"
+            placeholder="Enter company"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="positionName">Position Title</label>
+          <input
+            type="text"
+            name="positionName"
+            id="positionName"
+            placeholder="Enter position title"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="startDate">Start Date</label>
+          <input type="date" name="startDate" id="startDate" />
+          <label htmlFor="endDate">End Date</label>
+          <input type="date" name="endDate" id="endDate" />
+        </fieldset>
+      </div>
+      ;
+    </>
+  );
 }
 
 export default Experience;
