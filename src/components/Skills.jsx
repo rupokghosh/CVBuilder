e8b08e778ba80149a4e70c8fbc65631e68682afa
rupokghosh <rupokghosh291@gmail.com>
2024-02-03 15:36:04 -0800

    modify skills component for adding multiple entries

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,40 +1,35 @@
+// Skills.jsx
 import React, { useState, useEffect } from "react";
 
 function Skills({ setFormData }) {
-  const [skills, setSkills] = useState({
-    skill: "",
-  });
+  const [skill, setSkill] = useState("");
 
-  const handleInputChange = (fieldName, value) => {
-    setSkills((prevSkills) => ({
-      ...prevSkills,
-      [fieldName]: value,
-    }));
+  const addSkill = () => {
+    // Check if the skill is not an empty string before adding to CV
+    if (skill.trim() !== "") {
+      setFormData((prevData) => ({
+        ...prevData,
+        skills: [...prevData.skills, skill],
+      }));
+      setSkill("");
+    }
   };
 
-  useEffect(() => {
-    setFormData((prevData) => ({
-      ...prevData,
-      skills,
-    }));
-  }, [skills, setFormData]);
   return (
-    <>
-      <div className="skillInput">
-        <fieldset>
-          <label htmlFor="skill">Skill Name</label>
-          <input
-            type="text"
-            name="skill"
-            id="skill"
-            placeholder="Add skill"
-            value={skills.skill}
-            onChange={(e) => handleInputChange("skill", e.target.value)}
-          />
-        </fieldset>
-        <button>ADD</button>
-      </div>
-    </>
+    <div className="skillInput">
+      <fieldset>
+        <label htmlFor="skill">Skill Name</label>
+        <input
+          type="text"
+          name="skill"
+          id="skill"
+          value={skill}
+          onChange={(e) => setSkill(e.target.value)}
+          placeholder="Add skill"
+        />
+      </fieldset>
+      <button onClick={addSkill}>Add Skill</button>
+    </div>
   );
 }
 
